b6b257ac7b629924db0e98ff50b19802bc1b8451
sm_netrist <smainar@netrist.com>
2020-03-07 12:35:40 -0500

    sdfj

--- audio/delay_node.rs
+++ audio/delay_node.rs
@@ -1,4 +1,26 @@
-pub struct DelayNode{
+use node::{AudioNode, AudioParam};
+
+
+/* pub struct DelayNode{
    pub c: BaseAudioContext,
    pub option: DelayOptions
+} */
+
+pub struct DelayOptions{
+   pub maxDelayTime: f32;
+   pub delayTime: f32;
+}
+impl Default for DelayOptions{
+   fn default() -> Self{
+      DelayOptions{
+         maxDelayTime: 1,
+         delayTime: 0,
+      }
+   }
+}
+pub(crate) struct DelayNode{
+
+}
+impl DelayNode{
+   
 }
